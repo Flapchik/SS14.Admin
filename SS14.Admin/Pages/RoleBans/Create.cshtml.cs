ddf2d5cf45d2d9571449e860f32aa4d95513360e
Flapch <flapch@gmail.com>
2024-11-17 17:45:59 +0300

    Remove 'find last info' from role ban

--- SS14.Admin/Pages/RoleBans/Create.cshtml.cs
+++ SS14.Admin/Pages/RoleBans/Create.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using Content.Server.Database;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -27,6 +27,8 @@ namespace SS14.Admin.Pages.RoleBans
             public string? NameOrUid { get; set; }
             public string? IP { get; set; }
             public string? HWid { get; set; }
+            public bool UseLatestIp {get; set; }
+            public bool UseLatestHwid { get; set; }
             public int LengthMinutes { get; set; }
             [Required] public string Role { get; set; } = "";
             [Required] public string Reason { get; set; } = "";
@@ -63,12 +65,34 @@ namespace SS14.Admin.Pages.RoleBans
             {
                 RoleId = Input.Role.Trim()
             };
+			
+			var ipAddr = Input.IP;
+            var hwid = Input.HWid;
+			
+			if (Input.UseLatestHwid || Input.UseLatestIp)
+            {
+                if (string.IsNullOrWhiteSpace(Input.NameOrUid))
+                {
+                    TempData.SetStatusError("Must provide name/UID.");
+                    return Page();
+                }
+
+                var lastInfo = await _banHelper.GetLastPlayerInfo(Input.NameOrUid);
+                if (lastInfo == null)
+                {
+                    TempData.SetStatusError("Unable to find player");
+                    return Page();
+                }
+
+                ipAddr = Input.UseLatestIp ? lastInfo.Value.address.ToString() : Input.IP;
+                hwid = Input.UseLatestHwid ? (lastInfo.Value.hwid is { } h ? Convert.ToBase64String(h) : null) : Input.HWid;
+            }
 
             var error = await _banHelper.FillBanCommon(
                 roleBan,
                 Input.NameOrUid,
-                Input.IP,
-                Input.HWid,
+                ipAddr,
+                hwid,
                 Input.LengthMinutes,
                 Input.Reason);
 
@@ -86,3 +110,4 @@ namespace SS14.Admin.Pages.RoleBans
         }
     }
 }
+
